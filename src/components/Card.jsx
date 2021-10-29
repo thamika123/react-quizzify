3a3753c29337d701f22cdfeaad94e9b22a2d4a5f
thamika-git <adheesha@raditha.com>
2021-10-29 12:16:36 +0530

    Add click event handlers to the cards.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,11 @@
 function Card(props) {
     return (
-        <div className="bg-white shadow-lg rounded-2xl h-80 w-72 m-10 hover:shadow-xl">
+        <div
+            className="bg-white shadow-lg rounded-2xl h-80 w-72 m-10 hover:shadow-xl"
+            onClick={props.onClick}
+        >
             <div className="flex justify-center pt-12">
-                <img src={props.imgSrc} className="w-44 h-44" />
+                <img src={props.img} className="w-44 h-44" />
             </div>
             <h2 className="text-gray-800 font-sans font-medium text-xl pt-8 pl-8">
                 {props.title}
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM, { render } from "react-dom";
 
 import geoIcon from "./images/geography.svg";
 import histIcon from "./images/history.svg";
@@ -8,20 +8,41 @@ import spaceIcon from "./images/space.svg";
 
 import Card from "./components/Card";
 
-function App() {
+function Home(props) {
     return (
         <div>
-            <h1 class="text-3xl font-sans mt-60 font-bold text-gray-800 text-center">
+            <h1 className="text-3xl font-sans mt-60 font-bold text-gray-800 text-center">
                 Choose a category to get started:
             </h1>
-            <div class="flex flex-row flex-wrap justify-center mt-16">
-                <Card imgSrc={geoIcon} title="Geography" />
-                <Card imgSrc={histIcon} title="History" />
-                <Card imgSrc={sciIcon} title="Science" />
-                <Card imgSrc={spaceIcon} title="Space" />
+            <div className="flex flex-row flex-wrap justify-center mt-16">
+                <Card img={geoIcon} title="Geography" onClick={props.onClick} />
+                <Card img={histIcon} title="History" onClick={props.onClick} />
+                <Card img={sciIcon} title="Science" onClick={props.onClick} />
+                <Card img={spaceIcon} title="Space" onClick={props.onClick} />
             </div>
         </div>
     );
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { page: "home" };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({ page: "quiz" });
+    }
+
+    render() {
+        if (this.state.page === "home") {
+            return <Home onClick={this.handleClick} />;
+        }
+
+        return <div></div>;
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
