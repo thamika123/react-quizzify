4e6741f9555a109e9e88ea26d22a5f4af6542f4f
thamika-git <adheesha@raditha.com>
2021-10-29 17:31:16 +0530

    Add paths to purge option in Tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    purge: [],
+    purge: [
+        "./src/**/*.html",
+        "./src/**/*.jsx"
+    ],
     darkMode: false, // or 'media' or 'class'
     theme: {
         fontFamily: {
